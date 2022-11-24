0a5a53b46d55716029041f2288b4ca440b48c09b
lfrati <fratilapo@gmail.com>
2022-11-24 16:34:39 -0500

    Add pairs minimal subset evolution code + tests

--- src/paircosine/__init__.py
+++ src/paircosine/__init__.py
@@ -1,3 +1,4 @@
 from .cosine import *
+from .minimize import *
 
 __version__ = "0.2.1"
---
+++ tests/test_minimize.py
@@ -0,0 +1,37 @@
+from paircosine import *
+
+
+def test_min_subset():
+
+    # N = 1000  # population size
+    # M = 20  # dna size
+    # k = 200  # how many get to fuck (out of N)
+
+    N = 1024
+    distances = np.random.rand(N, N).astype(np.float32)
+    distances = np.maximum(distances, distances.transpose())
+
+    # import matplotlib.pyplot as plt
+    # plt.imshow(distances, interpolation="nearest")
+    # plt.show()
+
+    S = 16
+    locs = np.arange(N)
+    np.random.shuffle(locs)
+    xs = locs[:S]
+    for i in xs:
+        for j in xs:
+            distances[i, j] = -1
+
+    best, stats = select_best(distances, P=200, S=S, K=50, M=3, O=2, its=3_000)
+    # print("Final fit:", stats["fits"][-1])
+
+    # import matplotlib.pyplot as plt
+    # plt.plot(stats["fits"])
+    # plt.show()
+
+    missed = len(set(best).difference(set(xs)))
+    accuracy = 1 - missed / len(best)
+    print(accuracy)
+
+    assert accuracy > 0.5
